3ac67650b7d3c20e0d2761ea46ec6e954f9c3907
Kim Ommundsen <ko@deepmind.no>
2015-11-16 11:21:10 +0100

    gulpsass

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,34 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     $ = require('gulp-load-plugins')({ pattern: ['*'] });
 
-gulp.task('default', function() {});
+gulp.task('default', ['clean', 'sass', 'assemble', 'copy', 'fileExists', 'jshint']);
+
+gulp.task('clean', function () { });
+
+gulp.task('sass', function () {
+    return gulp.src('./src/scss/winstrap.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({ outputStyle: 'nested', precision: 5, includePaths:['node_modules'] }).on('error', $.sass.logError))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest('./distgulp/css'));
+});
+
+gulp.task('assemble', ['assemble:pages']);
+
+gulp.task('assemble:pages', function() {});
+
+gulp.task('copy', ['copy:assets', 'copy:doc']);
+
+gulp.task('copy:assets', ['copy:assets:files']);
+
+gulp.task('copy:assets:files', function () {});
+
+gulp.task('copy:doc', ['copy:doc:files']);
+
+gulp.task('copy:doc:files', function () {});
+
+gulp.task('watch', ['watch:sass', 'watch:doc']);
+
+gulp.task('watch:sass');
+
+gulp.task('watch:doc');
